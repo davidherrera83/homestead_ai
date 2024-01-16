4543a55a559ed63c03549c2378ae7b0cb720b824
David Herrera <david.herrera83@gmail.com>
2024-01-15 19:13:55 -0700

    Implementing more security

--- tests/api/test_assistant.py
+++ tests/api/test_assistant.py
@@ -1,5 +1,5 @@
 def test_homestead_io(openai):
-    response = openai.homestead("What should Herrera Homestead focus on as an emerging new business?qw  ")
+    response = openai.homestead("What should Herrera Homestead focus on as an emerging new business?")
     assistant_response = openai.parse_response(response)
     openai.save_test_artifact(data=assistant_response)
     assert response is not None
